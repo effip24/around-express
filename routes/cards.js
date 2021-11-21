3a4c9cf3f04b7d1062e0f0a6391992288d68b820
Ephraim Phil <effi188@gmail.com>
2021-11-21 20:56:14 +0200

    finished project

--- routes/cards.js
+++ routes/cards.js
@@ -1,14 +1,17 @@
 const router = require("express").Router();
 const fs = require("fs");
 const path = require("path");
+
 const filepath = path.join(__dirname, "../data/cards.json");
 
 router.get("/", (req, res) => {
   fs.readFile(filepath, { encoding: "utf8" }, (err, data) => {
-    err
-      ? res.status(500) &&
-        res.send({ message: "An error has occurred on the server" })
-      : res.send(JSON.parse(data));
+    if (err) {
+      res.status(500);
+      res.send({ message: "An error has occurred on the server" });
+      return;
+    }
+    res.send(JSON.parse(data));
   });
 });
 
--- routes/users.js
+++ routes/users.js
@@ -1,28 +1,39 @@
 const router = require("express").Router();
 const fs = require("fs");
 const path = require("path");
+
 const filepath = path.join(__dirname, "../data/users.json");
 
 router.get("/", (req, res) => {
   fs.readFile(filepath, { encoding: "utf8" }, (err, data) => {
-    err
-      ? res.status(500) &&
-        res.send({ message: "An error has occurred on the server" })
-      : res.send(JSON.parse(data));
+    if (err) {
+      res.status(500);
+      res.send({ message: "An error has occurred on the server" });
+      return;
+    }
+
+    res.send(JSON.parse(data));
   });
 });
 
 router.get("/:id", (req, res) => {
   fs.readFile(filepath, { encoding: "utf8" }, (err, data) => {
-    const user = JSON.parse(data).filter(
+    if (err) {
+      res.status(500);
+      res.send({ message: "An error has occurred on the server" });
+      return;
+    }
+
+    const userData = JSON.parse(data).filter(
       (user) => user._id === req.params.id
     )[0];
-    err
-      ? res.status(500) &&
-        res.send({ message: "An error has occurred on the server" })
-      : user
-      ? res.send(user)
-      : res.status(400) && res.send({ message: "User ID not found" });
+    if (!userData) {
+      res.status(400);
+      res.send({ message: "User ID not found" });
+      return;
+    }
+
+    res.send(userData);
   });
 });
 
